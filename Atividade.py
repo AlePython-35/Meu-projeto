68dfaaa1561fe870ceac19ad58a81963b3f33cc1
AlePython-35 <mariaalluizaa01@gmail.com>
2024-09-25 11:49:49 -0300

    atualizando

---
+++ Atividade.py
@@ -0,0 +1,15 @@
+# Atividade 3...
+X = int(input("Digite o valor de X: "))
+Y = int(input("Digite o valor de Y: "))
+Z = int(input("Digite o valor de z:" ))
+
+
+if X < Y and Y < Z:
+   print("Ordem crescente!")
+elif X > Y and Y > Z:
+    print("Ordem decrescente!")
+else:
+    print("Misturados!")
+
+
+
