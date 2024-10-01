a9cc8f86b5c4785d4e2b4e105f2a710136fc1cb3
John Doe <johndoe@example.com>
2024-10-01 11:46:19 -0300

    Att

--- Prog0201.py
+++ Prog0201.py
@@ -1,1 +1,27 @@
-# 
+# Escalação de jogadores...
+JOGADORES = []
+NUMEROS = []
+
+for X in range(0,11):
+    NOME = input(f"Digite o nome do jogador:")
+    JOGADORES.append(NOME)
+    CAMISA = input("Digite o número da camisa:")
+    NUMEROS.append(CAMISA)
+
+TROCA = input("Você que fazer substituições") 
+if TROCA == "S":
+   for X in range(0, 3):
+    NOME = input("Digite o nome do jogador substituido") 
+    CAMISA = input("Digite o seu respectivo número: ") 
+    JOGADORES.remove(NOME)
+    NUMEROS.remove(CAMISA)
+    NOME = input("Digite i nome do jogador substituto: ")
+    CAMISA = input("Digite o seu respectivo número: ")
+    JOGADORES.append(NOME)
+    NUMEROS.append(CAMISA)
+
+print("Lista dos jogadores Atualiza")
+for Y in range(0, 11):
+    print(f"{NUMEROS[Y]}. {JOGADORES[Y]}")
+       
+        
---
+++ Prog0301.py
@@ -0,0 +1,9 @@
+# Cadastra tarefas
+TAREFAS = []
+
+for i in range(0,5):
+        TAREFAS = input(f"Digite a descrição da tarefa: ")
+        TAREFAS.append(TAREFAS)
+        print(f"Arrumar o quarto: {TAREFAS[0]}")
+    
+
